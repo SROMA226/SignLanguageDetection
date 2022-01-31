7004b4adbb590bf905647240f2c3b30950c840e3
Romaric Nana <sidnanaroma@gmail.com>
2022-01-31 13:04:09 +0000

    ecriture du script train model SVC

---
+++ scripts/train_model.py
@@ -0,0 +1,31 @@
+import pickle # Serialiser des objets (y comporis des modeles)
+
+import pandas as pd
+#from sklearn.svm import LinearSVC
+from sklearn.svm import SVC 
+
+from config import Config
+
+Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
+
+X_train_ML = pd.read_csv(str(Config.FEATURES_PATH / "x_train_features_ml.csv"))
+Y_train_ML = np.load(str(Config.FEATURES_PATH / "y_train_labels_ml.npy"), allow_pickle=False)
+
+# Entrainement du model
+base_model_svc = SVC(kernel="rbf")
+legacy_random_state = np.random.RandomState()
+modelSVC = HalvingRandomSearchCV(
+    base_model_svc,
+    param_distributions={
+        "C": scipy.stats.expon(scale=100),
+        "gamma": scipy.stats.expon(scale=0.1),
+        "class_weight": ["balanced", None],
+    },
+    cv=3,
+    n_jobs=3,
+    random_state=legacy_random_state,
+    verbose=0,
+).fit(X_train_ML, Y_train_ML)
+
+# Enregisrement du model
+pickle.dump(modelSVC, open(str(Config.MODELS_PATH / "model.pk"), mode='wb'))
