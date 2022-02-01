b752dbdf204a4fbd4f8ad4bba1ba0acea47d26b3
Romaric Nana <sidnanaroma@gmail.com>
2022-02-01 17:23:18 +0000

    ecriture du script train and evaluate model CNN

--- scripts/eval_model.py
+++ scripts/eval_model.py
@@ -24,20 +24,21 @@ Y_true = np.argmax(Y_test_CNN,axis = 1)
 
 #print(classification_report(Y_true, Y_pred_class))
 
+evaluation = modelCNN.evaluate(X_test_CNN, Y_test_CNN)
+
 with open(str(Config.ASSETS_PATH)+'/ metrique_cnn.txt', 'w') as f:
     f.write(classification_report(Y_true, Y_pred_class))
 
+with open(str(Config.METRICS_FILE_PATH), mode='w') as f:
+    json.dump(dict(validation_accuracy=evaluation[1]), f)
+
+"""
 y_pred = modelCNN.predict(X_test_CNN)
 test_fscore = f1_score(y_true=Y_test_CNN, y_pred=y_pred, average='weighted')
 test_precision = precision_score(y_true=Y_test_CNN, y_pred=y_pred, average='weighted')
 test_accuracy = accuracy_score(y_true=Y_test_CNN, y_pred=y_pred)
 test_recall = recall_score(y_true=Y_test_CNN, y_pred=y_pred, average='weighted')
 
-evaluation = modelCNN.evaluate(X_test_CNN, Y_test_CNN)
-
-
 with open(str(Config.METRICS_FILE_PATH), mode='w') as f:
     json.dump(dict(f1_score=test_fscore, precision=test_precision, recall_score=test_recall,accuracy_score=test_accuracy), f)
-
-with open(str(Config.METRICS_FILE_PATH), mode='w') as f:
-    json.dump(dict(validation_accuracy=evaluation[1]), f)
+"""
